1fba5fb2bb530eb1d9b6434c2d4965b6cc027abd
calmez0609 <33241538+calmez0609@users.noreply.github.com>
2018-11-29 23:37:03 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('DuNPe+CYIv7cH5PqO8lp7a0S7+HsBf7g2L3i/aG6dNTjQLFoIcE0dhjdcrJlkG6tFb9KF3155ZvNcj159osT9SDdKY+q1iTQvy+AmeN+S2riPnP19mwH17TV1Wca1jzAjAyFKWyvn9iU1JGPtTusaQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('V8J6CcKNRsHZF9pVXzV02sTJ8pYQlU1B3kcXZZ1y9dlP4/QvNf2nWtwsyFLmrSTEgcgsV8Hn4W1RaGjkdQtrhWZK/QemcOwEHCK31GDj4mpkICb8OW5HRlZ3wYzXWWdJYz+Zb+mGxj9yDZW8N5tb4QdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('9cd07218b8f805ec98f384d14b47ace7')
+handler = WebhookHandler('fcd25fa436e9474a1213390b2e674ce3')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -31,17 +31,14 @@ def callback():
     return 'OK'
 
 # 處理訊息
-def function(text):
-    list=['畜生','王八蛋','龜孫']
-    if text=='隆基是':
-        text=random.choice(list)
-    return text
-    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(function(event.message.text))
+    message = TextSendMessage(text = Reply(event.message.text))
     line_bot_api.reply_message(event.reply_token, message)
 
+def Reply(text):
+    if text == "hi" :
+        return 'hello'
 
 import os
 if __name__ == "__main__":
