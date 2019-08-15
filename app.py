8f074bd442fe0581dbd220bbe46c57beb7102b0d
calmez0609 <33241538+calmez0609@users.noreply.github.com>
2019-08-16 01:17:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def function(text):
     elif text=='喔主耶穌' or '喔～主耶穌':
         text='阿們，哈雷路亞!'
     elif text=='時間':
-        text=''+str(datetime.date.today().strftime("%Y%m%d"))
+today = datetime.date.today().strftime("%Y%m%d")
+        response = line_bot_api.get_insight_message_delivery(today)
+        text=''+str(response)
     else:
         text=''
     return text
